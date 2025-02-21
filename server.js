7737cbcb99593338d739a8392a3ac44721648d4e
SATYAM-150506 <156522371+SATYAM-150506@users.noreply.github.com>
2025-02-21 18:58:01 +0530

    20/02/2025
    post_server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+//if any request changes the state of any server then we have to do it by post request
+//data is not visible in post request in url
+//data is send through req.body
+const express=require("express");
+const app=express();
+
+app.get("/signup",(req,res)=>{
+    res.sendFile(__dirname+"/signup.html");
+})
+app.post("/signup",(req,res)=>{
+    let {username,useremail,userpassword}=req.body;
+    console.log(username,useremail,userpassword);
+})
+app.listen(3456,()=>{
+    console.log("server started..");
+})
