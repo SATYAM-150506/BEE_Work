663ec6dc81b43f4fcf1893a7f719940b3595fd48
SATYAM-150506 <156522371+SATYAM-150506@users.noreply.github.com>
2025-02-21 19:30:15 +0530

    21/02/2025
    post data to array

--- server.js
+++ server.js
@@ -3,13 +3,22 @@
 //data is send through req.body
 const express=require("express");
 const app=express();
-
+//middleware to parse urlencoded data
+app.use(express.urlencoded({extended:true}))
+let userData=[]
 app.get("/signup",(req,res)=>{
     res.sendFile(__dirname+"/signup.html");
 })
 app.post("/signup",(req,res)=>{
     let {username,useremail,userpassword}=req.body;
-    console.log(username,useremail,userpassword);
+    let newuser={
+        name:username,
+        email:useremail,
+        password:userpassword
+    }
+    userData.push(newuser);
+    res.send(userData);
+    // console.log(username,useremail,userpassword);
 })
 app.listen(3456,()=>{
     console.log("server started..");
